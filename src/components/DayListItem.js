36be55253f68a1eeb2e6279a86339dd1070c0f5f
Kevin Laburte <kevin.laburte@gmail.com>
2019-11-25 12:50:58 -0500

    Add dayListItem component

---
+++ src/components/DayListItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import 'components/DayListItem.scss';
+
+export default function DayListItem({ name, spots, selected, setDay }) {
+  return (
+    <li className={classNames('day-list__item', { 'day-list__item--selected': selected, 'day-list__item--full': spots === 0 })} onClick={() => setDay && setDay(name)}>
+      <h2 className='text--regular'>{name}</h2> 
+      <h3 className='text--light'>{spots}</h3>
+    </li>
+  );
+}
