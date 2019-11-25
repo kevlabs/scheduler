fcccb1597479aa438b39ed855bf7adb71bf7e408
Kevin Laburte <kevin.laburte@gmail.com>
2019-11-24 21:47:48 -0500

    Add button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React from 'react';
+import classNames from 'classnames';
 
-import "components/Button.scss";
+import 'components/Button.scss';
 
 export default function Button(props) {
-   return <></>;
+   return <button
+      className={classNames('button', { 'button--confirm': props.confirm, 'button--danger': props.danger })}
+      onClick={props.onClick}
+      disabled={props.disabled}
+   >{props.children}</button>;
 }
