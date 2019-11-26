fead4cd3495ee5432cf90fb141b07bf0dce7218f
Kevin Laburte <kevin.laburte@gmail.com>
2019-11-26 16:11:35 -0500

    Fetch days data from API

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
 
 // test data
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
 
 const appointments = [
   {
@@ -68,7 +69,14 @@ const appointments = [
 ];
 
 export default function Application(props) {
+  const [days, setDays] = useState([]);
   const [day, setDay] = useState('Monday');
+
+  useEffect(function fetchDays() {
+    axios.get('api/days')
+      .then(({ data }) => setDays(data))
+      .catch(err => console.log(`Error: ${err.message}`));
+  }, []);
 
   return (
     <main className="layout">
