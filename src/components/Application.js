bab6629e65ac209d5ad5674b0f13a9a2ff9dfe85
Kevin Laburte <kevin.laburte@gmail.com>
2019-11-27 18:23:40 -0500

    Add error handling

--- src/components/Application.js
+++ src/components/Application.js
@@ -100,9 +100,25 @@ export default function Application(props) {
 
     // push appointment to db and update state if successful
     return axios.put(`api/appointments/${id}`, appointment)
-      .then(() => setState(prev => ({ ...prev, appointments })))
-      .catch(err => console.log(`Error: ${err.message}`));
+      .then(() => setState(prev => ({ ...prev, appointments })));
   }
+
+  const cancelInterview = (id) => {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    }
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    // delete interview from db and update state if successful
+    return axios.delete(`api/appointments/${id}`)
+      .then(() => setState(prev => ({ ...prev, appointments })));
+  }
+  
 
   return (
     <main className="layout">
@@ -125,7 +141,7 @@ export default function Application(props) {
       />
       </section>
       <section className="schedule">
-        {getAppointmentsForDay(state, state.day).map(appointment => <Appointment key={appointment.id} {...{...appointment, interview: getInterview(state, appointment.interview), interviewers: getInterviewersForDay(state, state.day), bookInterview}} />)}
+        {getAppointmentsForDay(state, state.day).map(appointment => <Appointment key={appointment.id} {...{...appointment, interview: getInterview(state, appointment.interview), interviewers: getInterviewersForDay(state, state.day), bookInterview, cancelInterview }} />)}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,50 +5,80 @@ import Show from 'components/Appointment/Show';
 import Empty from 'components/Appointment/Empty';
 import Form from 'components/Appointment/Form';
 import Status from 'components/Appointment/Status';
+import Error from 'components/Appointment/Error';
+import Confirm from 'components/Appointment/Confirm';
 import useVisualMode from 'hooks/useVisualMode';
 import 'components/Appointment/styles.scss';
 import { statement } from '@babel/template';
 
-export default function Appointment({ id, time, interview, interviewers, bookInterview }) {
+export default function Appointment({ id, time, interview, interviewers, bookInterview, cancelInterview }) {
 
   // declare views enum
   const views = {
     EMPTY: 'EMPTY',
     SHOW: 'SHOW',
     CREATE: 'CREATE',
+    EDIT: 'EDIT',
     SAVING: 'SAVING',
-    CONFIRM: 'CONFIRM'
+    DELETING: 'DELETING',
+    CONFIRM: 'CONFIRM',
+    ERROR_SAVING: 'ERROR SAVING',
+    ERROR_DELETING: 'ERROR DELETING'
   }
   
   const view = useVisualMode(interview ? views.SHOW : views.EMPTY);
   
-  // save form
+  // save appointment
   const save = (name, interviewer) => {
     const interview = {
       student: name,
       interviewer
     };
     view.transition(views.SAVING)
-    bookInterview(id, interview).then(() => view.transition(views.SHOW));
-  }
+    bookInterview(id, interview)
+      .then(() => view.transition(views.SHOW))
+      .catch(err => view.transition(views.ERROR_SAVING, true));
+  };
+
+  // cancel appointment
+  const cancel = (id) => {
+    view.transition(views.DELETING, true)
+    cancelInterview(id)
+      .then(() => view.transition(views.EMPTY))
+      .catch(err => view.transition(views.ERROR_DELETING, true));
+  };
+
+  // generic cancel
+  const onCancel = () => view.back();
 
   // empty
   const onAdd = () => view.transition(views.CREATE);
-  // show
-  const onEdit = () => view.transition(views.SAVING);
-  const onDelete = () => view.transition(views.CONFIRM);
 
   // form
-  const onCancel = () => view.back();
   const onSave = (name, interviewer) => save(name, interviewer);
+
+  // show
+  const onEdit = () => view.transition(views.EDIT);
+  const onDelete = () => view.transition(views.CONFIRM);
+
+  // error
+  const onClose = onCancel;
+
+  // confirm
+  const onConfirm = () => cancel(id);
 
   return (
     <article className='appointment'>
       <Header {...{ time }} />
       { view.mode === views.EMPTY && <Empty {...{ onAdd }} />}
       { view.mode === views.SHOW && <Show {...{ ...interview, onEdit, onDelete }} />}
-      { view.mode === views.CREATE && <Form {...{ ...interview, interviewers, onCancel, onSave }} />}
+      { view.mode === views.CREATE && <Form {...{ interviewers, onCancel, onSave }} />}
+      { view.mode === views.EDIT && <Form {...{ name: interview.student, interviewer: interview.interviewer, interviewers, onCancel, onSave }} />}
       { view.mode === views.SAVING && <Status message='Saving...' />}
+      { view.mode === views.DELETING && <Status message='Deleting...' />}
+      { view.mode === views.ERROR_SAVING && <Error {...{ message: 'Error while saving', onClose }} />}
+      { view.mode === views.ERROR_DELETING && <Error {...{ message: 'Error while deleting', onClose }} />}
+      { view.mode === views.CONFIRM && <Confirm {...{ message: 'Are you sure?', onCancel, onConfirm }} />}
     </article>
   );
 
